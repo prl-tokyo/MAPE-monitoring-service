4edd4356ca88b1f02270c41d1c4490fa1d5c68da
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-24 23:01:53 +0900

    Import generated Spring project

---
+++ src/main/java/jp/ac/nii/prl/mape/monitoring/MapeMonitoringServiceApplication.java
@@ -0,0 +1,12 @@
+package jp.ac.nii.prl.mape.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapeMonitoringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapeMonitoringServiceApplication.class, args);
+	}
+}
