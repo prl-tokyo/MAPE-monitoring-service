53e7d61cf928772c29c26e756a27561b5983fae8
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-25 19:53:03 +0900

    Get load for an instance

--- src/main/java/jp/ac/nii/prl/mape/monitoring/service/CloudWatchService.java
+++ src/main/java/jp/ac/nii/prl/mape/monitoring/service/CloudWatchService.java
@@ -2,4 +2,6 @@ package jp.ac.nii.prl.mape.monitoring.service;
 
 public interface CloudWatchService {
 
+	Double getLoad(String instId);
+
 }
--- src/main/java/jp/ac/nii/prl/mape/monitoring/service/CloudWatchServiceImpl.java
+++ src/main/java/jp/ac/nii/prl/mape/monitoring/service/CloudWatchServiceImpl.java
@@ -1,10 +1,15 @@
 package jp.ac.nii.prl.mape.monitoring.service;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Service;
 
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.cloudwatch.AmazonCloudWatchClient;
+import com.amazonaws.services.cloudwatch.model.Dimension;
+import com.amazonaws.services.cloudwatch.model.GetMetricStatisticsRequest;
+import com.amazonaws.services.cloudwatch.model.GetMetricStatisticsResult;
 
 @Service("cloudWatchService")
 public class CloudWatchServiceImpl implements CloudWatchService {
@@ -15,5 +20,24 @@ public class CloudWatchServiceImpl implements CloudWatchService {
 		cwClient = Region
 				.getRegion(Regions.AP_NORTHEAST_1)
 				.createClient(AmazonCloudWatchClient.class, null, null);
+	}
+	
+	@Override
+	public Double getLoad(String instId) {
+		long offset = 1000 * 60 * 15; // 15 minutes
+		GetMetricStatisticsRequest request = new GetMetricStatisticsRequest()
+				.withStartTime(new Date(new Date().getTime() - offset))
+				.withEndTime(new Date())
+				.withNamespace("AWS/EC2")
+				.withDimensions(new Dimension().withName("InstanceId").withValue(instId))
+				.withPeriod(60*15)
+				.withMetricName("CPUUtilization")
+				.withStatistics("Average");
+		
+		GetMetricStatisticsResult result = cwClient.getMetricStatistics(request);
+		
+		System.out.println(result.toString());
+		
+		return result.getDatapoints().get(0).getAverage();
 	}
 }
