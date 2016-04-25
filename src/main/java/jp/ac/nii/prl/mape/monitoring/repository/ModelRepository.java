13dff548779b0645b77e261b468e92e6f91d5081
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-25 15:17:21 +0900

    findById() method

--- src/main/java/jp/ac/nii/prl/mape/monitoring/repository/ModelRepository.java
+++ src/main/java/jp/ac/nii/prl/mape/monitoring/repository/ModelRepository.java
@@ -1,9 +1,13 @@
 package jp.ac.nii.prl.mape.monitoring.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import jp.ac.nii.prl.mape.monitoring.model.Model;
 
 public interface ModelRepository extends JpaRepository<Model, Long> {
 
+	Optional<Model> findById(Long modelId);
+	
 }
