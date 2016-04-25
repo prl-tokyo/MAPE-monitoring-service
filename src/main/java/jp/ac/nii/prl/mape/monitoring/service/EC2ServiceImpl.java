4d796d5ff37ddc605d7f9dded46fb3bc70f2f936
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-25 19:55:38 +0900

    Remove unused imports

--- src/main/java/jp/ac/nii/prl/mape/monitoring/service/EC2ServiceImpl.java
+++ src/main/java/jp/ac/nii/prl/mape/monitoring/service/EC2ServiceImpl.java
@@ -3,7 +3,6 @@ package jp.ac.nii.prl.mape.monitoring.service;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.amazonaws.regions.Region;
--- src/main/java/jp/ac/nii/prl/mape/monitoring/service/SecurityGroupServiceImpl.java
+++ src/main/java/jp/ac/nii/prl/mape/monitoring/service/SecurityGroupServiceImpl.java
@@ -1,14 +1,10 @@
 package jp.ac.nii.prl.mape.monitoring.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.amazonaws.services.ec2.model.IpPermission;
 
-import jp.ac.nii.prl.mape.monitoring.model.FirewallRule;
 import jp.ac.nii.prl.mape.monitoring.model.SecurityGroup;
 
 @Service("securityGroupService")
