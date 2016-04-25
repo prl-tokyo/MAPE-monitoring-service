f00f8b4f3fb19dedac0fc4d5d747d384c61e399f
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-25 19:21:30 +0900

    CloudWatchService constructor

--- src/main/java/jp/ac/nii/prl/mape/monitoring/service/CloudWatchServiceImpl.java
+++ src/main/java/jp/ac/nii/prl/mape/monitoring/service/CloudWatchServiceImpl.java
@@ -2,6 +2,8 @@ package jp.ac.nii.prl.mape.monitoring.service;
 
 import org.springframework.stereotype.Service;
 
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.cloudwatch.AmazonCloudWatchClient;
 
 @Service("cloudWatchService")
@@ -10,6 +12,8 @@ public class CloudWatchServiceImpl implements CloudWatchService {
 	private AmazonCloudWatchClient cwClient;
 	
 	public CloudWatchServiceImpl() {
-		
+		cwClient = Region
+				.getRegion(Regions.AP_NORTHEAST_1)
+				.createClient(AmazonCloudWatchClient.class, null, null);
 	}
 }
