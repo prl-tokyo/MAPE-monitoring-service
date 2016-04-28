fb7f3ac72f5848104d0a1fe7c2a658117b950171
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-28 15:49:09 +0900

    Return load of 0 if no datapoints are available

--- src/main/java/jp/ac/nii/prl/mape/monitoring/service/CloudWatchServiceImpl.java
+++ src/main/java/jp/ac/nii/prl/mape/monitoring/service/CloudWatchServiceImpl.java
@@ -1,12 +1,14 @@
 package jp.ac.nii.prl.mape.monitoring.service;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.cloudwatch.AmazonCloudWatchClient;
+import com.amazonaws.services.cloudwatch.model.Datapoint;
 import com.amazonaws.services.cloudwatch.model.Dimension;
 import com.amazonaws.services.cloudwatch.model.GetMetricStatisticsRequest;
 import com.amazonaws.services.cloudwatch.model.GetMetricStatisticsResult;
@@ -37,7 +39,9 @@ public class CloudWatchServiceImpl implements CloudWatchService {
 		GetMetricStatisticsResult result = cwClient.getMetricStatistics(request);
 		
 		System.out.println(result.toString());
-		
+		List<Datapoint> datapoints = result.getDatapoints();
+		if (datapoints.isEmpty())
+			return 0.0;
 		return result.getDatapoints().get(0).getAverage();
 	}
 }
