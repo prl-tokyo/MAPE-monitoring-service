de744ed007062e54c53f185f856eb81f636003d5
Lionel Montrieux <lionel@nii.ac.jp>
2016-08-21 19:35:11 +0900

    Add response time and sort members

--- src/main/java/jp/ac/nii/prl/mape/monitoring/model/Instance.java
+++ src/main/java/jp/ac/nii/prl/mape/monitoring/model/Instance.java
@@ -50,18 +50,19 @@ public class Instance {
 	
 	private Double load;
 	
+	private Integer instResponseTime;
 	
 	private String architecture;
-	
+
 	@OneToMany(mappedBy="instance")
 	private List<InstanceBlockDeviceMapping> blockDeviceMapping;
 
 	private String clientToken;
-	
+
 	private Boolean enaSupport;
 	
 	private String hypervisor;
-	
+
 	@OneToOne
 	private IamInstanceProfile iamInstanceProfile;
 	
@@ -70,40 +71,92 @@ public class Instance {
 	private String kernelId;
 	
 	private String keyName;
-
+	
 	@OneToMany(mappedBy="instance")
 	private List<InstanceNetworkInterface> networkInterface;
 	
 	@OneToOne
 	private Placement placement;
-
 	
 	@JsonIgnore
 	@ManyToOne
 	private Model model;
-	
-	public String getPlatform() {
-		return platform;
-	}
 
 	public String getAmi() {
 		return ami;
+	}
+	
+	public Integer getAmiLaunchIndex() {
+		return amiLaunchIndex;
+	}
+
+	
+	public String getArchitecture() {
+		return architecture;
+	}
+	
+	public List<InstanceBlockDeviceMapping> getBlockDeviceMapping() {
+		return blockDeviceMapping;
+	}
+
+	public List<InstanceBlockDeviceMapping> getBlockDeviceMappings() {
+		return blockDeviceMapping;
+	}
+
+	public String getClientToken() {
+		return clientToken;
+	}
+
+	public Boolean getEbsOptimized() {
+		return ebsOptimized;
+	}
+
+	public Boolean getEnaSupport() {
+		return enaSupport;
+	}
+
+	public String getHypervisor() {
+		return hypervisor;
+	}
+
+	public IamInstanceProfile getIamInstanceProfile() {
+		return iamInstanceProfile;
 	}
 
 	public Long getId() {
 		return id;
 	}
 
+	public String getImageId() {
+		return imageId;
+	}
+
 	public String getInstID() {
 		return instID;
 	}
-
+	
+	public Integer getInstResponseTime() {
+		return instResponseTime;
+	}
+	
 	public Integer getInstStatus() {
 		return instStatus;
 	}
 
 	public String getInstType() {
 		return instType;
+	}
+
+	public String getKernelId() {
+		return kernelId;
+	}
+
+	public String getKeyName() {
+		return keyName;
+	}
+
+	public Date getLaunchTime() {
+		return launchTime;
 	}
 
 	public Double getLoad() {
@@ -114,6 +167,26 @@ public class Instance {
 		return model;
 	}
 
+	public Monitoring getMonitoring() {
+		return monitoring;
+	}
+
+	public List<InstanceNetworkInterface> getNetworkInterface() {
+		return networkInterface;
+	}
+	
+	public Placement getPlacement() {
+		return placement;
+	}
+	
+	public String getPlatform() {
+		return platform;
+	}
+	
+	public String getRootDeviceName() {
+		return rootDeviceName;
+	}
+
 	public String getSecurityGroupRef() {
 		return securityGroupRef;
 	}
@@ -121,11 +194,7 @@ public class Instance {
 	public Integer getState() {
 		return state;
 	}
-	
-	public void setPlatform(String platform) {
-		this.platform = platform;
-	}
-	
+
 	public List<Tag> getTags() {
 		return tags;
 	}
@@ -134,12 +203,52 @@ public class Instance {
 		this.ami = ami;
 	}
 
+	public void setAmiLaunchIndex(Integer amiLaunchIndex) {
+		this.amiLaunchIndex = amiLaunchIndex;
+	}
+
+	public void setArchitecture(String architecture) {
+		this.architecture = architecture;
+	}
+
+	public void setBlockDeviceMapping(List<InstanceBlockDeviceMapping> blockDeviceMapping) {
+		this.blockDeviceMapping = blockDeviceMapping;
+	}
+
+	public void setClientToken(String clientToken) {
+		this.clientToken = clientToken;
+	}
+
+	public void setEbsOptimized(Boolean ebsOptimized) {
+		this.ebsOptimized = ebsOptimized;
+	}
+
+	public void setEnaSupport(Boolean enaSupport) {
+		this.enaSupport = enaSupport;
+	}
+
+	public void setHypervisor(String hypervisor) {
+		this.hypervisor = hypervisor;
+	}
+
+	public void setIamInstanceProfile(IamInstanceProfile iamInstanceProfile) {
+		this.iamInstanceProfile = iamInstanceProfile;
+	}
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
 
+	public void setImageId(String imageId) {
+		this.imageId = imageId;
+	}
+
 	public void setInstID(String instID) {
 		this.instID = instID;
+	}
+
+	public void setInstResponseTime(Integer instResponseTime) {
+		this.instResponseTime = instResponseTime;
 	}
 
 	public void setInstStatus(Integer instStatus) {
@@ -150,6 +259,18 @@ public class Instance {
 		this.instType = instType;
 	}
 
+	public void setKernelId(String kernelId) {
+		this.kernelId = kernelId;
+	}
+
+	public void setKeyName(String keyName) {
+		this.keyName = keyName;
+	}
+
+	public void setLaunchTime(Date launchTime) {
+		this.launchTime = launchTime;
+	}
+
 	public void setLoad(Double load) {
 		this.load = load;
 	}
@@ -158,149 +279,40 @@ public class Instance {
 		this.model = model;
 	}
 
-	public void setSecurityGroupRef(String securityGroupRef) {
-		this.securityGroupRef = securityGroupRef;
-	}
-	
-	public void setState(Integer state) {
-		this.state = state;
-	}
-	
-	public void setTags(List<Tag> tags) {
-	// TODO Auto-generated method stub
-		this.tags =  tags;
-	}
-	
-	public Date getLaunchTime() {
-		return launchTime;
-	}
-
-	public void setLaunchTime(Date launchTime) {
-		this.launchTime = launchTime;
-	}
-
-	public Monitoring getMonitoring() {
-		return monitoring;
-	}
-
 	public void setMonitoring(Monitoring monitoring) {
 		this.monitoring = monitoring;
-	}
-
-	public Boolean getEbsOptimized() {
-		return ebsOptimized;
-	}
-
-	public void setEbsOptimized(Boolean ebsOptimized) {
-		this.ebsOptimized = ebsOptimized;
-	}
-
-	public String getRootDeviceName() {
-		return rootDeviceName;
-	}
-
-	public void setRootDeviceName(String rootDeviceName) {
-		this.rootDeviceName = rootDeviceName;
-	}
-
-	public Instance withRootDeviceName(String rootDeviceName){
-		return this;
-	}
-
-	public Integer getAmiLaunchIndex() {
-		return amiLaunchIndex;
-	}
-
-	public void setAmiLaunchIndex(Integer amiLaunchIndex) {
-		this.amiLaunchIndex = amiLaunchIndex;
-	}
-
-	public String getArchitecture() {
-		return architecture;
-	}
-
-	public void setArchitecture(String architecture) {
-		this.architecture = architecture;
-	}
-	
-	public List<InstanceBlockDeviceMapping> getBlockDeviceMappings() {
-		return blockDeviceMapping;
-	}
-
-	public void setBlockDeviceMapping(List<InstanceBlockDeviceMapping> blockDeviceMapping) {
-		this.blockDeviceMapping = blockDeviceMapping;
-	}
-
-	public String getClientToken() {
-		return clientToken;
-	}
-
-	public void setClientToken(String clientToken) {
-		this.clientToken = clientToken;
-	}
-
-	public Boolean getEnaSupport() {
-		return enaSupport;
-	}
-
-	public void setEnaSupport(Boolean enaSupport) {
-		this.enaSupport = enaSupport;
-	}
-
-	public String getHypervisor() {
-		return hypervisor;
-	}
-
-	public void setHypervisor(String hypervisor) {
-		this.hypervisor = hypervisor;
-	}
-
-	public IamInstanceProfile getIamInstanceProfile() {
-		return iamInstanceProfile;
-	}
-
-	public void setIamInstanceProfile(IamInstanceProfile iamInstanceProfile) {
-		this.iamInstanceProfile = iamInstanceProfile;
-	}
-
-	public String getImageId() {
-		return imageId;
-	}
-
-	public void setImageId(String imageId) {
-		this.imageId = imageId;
-	}
-
-	public String getKernelId() {
-		return kernelId;
-	}
-
-	public void setKernelId(String kernelId) {
-		this.kernelId = kernelId;
-	}
-
-	public String getKeyName() {
-		return keyName;
-	}
-
-	public void setKeyName(String keyName) {
-		this.keyName = keyName;
-	}
-
-	public List<InstanceNetworkInterface> getNetworkInterface() {
-		return networkInterface;
 	}
 
 	public void setNetworkInterface(List<InstanceNetworkInterface> networkInterface) {
 		this.networkInterface = networkInterface;
 	}
 
-	public Placement getPlacement() {
-		return placement;
-	}
-
 	public void setPlacement(Placement placement) {
 		this.placement = placement;
+	}
+
+	public void setPlatform(String platform) {
+		this.platform = platform;
+	}
+
+	public void setRootDeviceName(String rootDeviceName) {
+		this.rootDeviceName = rootDeviceName;
+	}
+
+	public void setSecurityGroupRef(String securityGroupRef) {
+		this.securityGroupRef = securityGroupRef;
+	}
+
+	public void setState(Integer state) {
+		this.state = state;
+	}
+
+	public void setTags(List<Tag> tags) {
+		this.tags =  tags;
+	}
+
+	public Instance withRootDeviceName(String rootDeviceName){
+		return this;
 	}
 
 }
